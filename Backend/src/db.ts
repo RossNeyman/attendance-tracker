cd51a79686ecd87d93c9be489436c892c7e5f15b
RossNeyman <ross.neyman@gmail.com>
2025-04-20 23:25:17 -0400

    WIP getting backend to run & tests

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,9 +1,9 @@
 import {getFirestore} from "firebase-admin/firestore";
 import {initializeApp, applicationDefault, cert} from "firebase-admin/app";
+import {ServiceAccount} from "firebase-admin";
+import serviceAccount from "../serviceAccountKey.json" with {type: "json"};
 
-//Initialize Firebase Admin SDK with service account credentials
-const serviceAccount = require("../serviceAccountKey.json");
-initializeApp({credential: cert(serviceAccount)});
+initializeApp({credential: cert(serviceAccount as ServiceAccount)});
 
 //Export singleton db connection
 const db = getFirestore(); 
