7f6ea042a44cc30f1d5625394f2030715b78d2d5
RossNeyman <ross.neyman@gmail.com>
2025-05-03 10:49:15 -0400

    WIP 2

--- Frontend/src/features/logsSlice.ts
+++ Frontend/src/features/logsSlice.ts
@@ -2,13 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const logsApi = createApi({
   reducerPath: 'logsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: '/api', 
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: '/logs' }),
   endpoints: (builder) => ({
     logAttendance: builder.mutation({
       query: ({ userId, roomId, weekId, email }) => ({
-        url: `/logs`,
+        url: `/`,
         method: 'POST',
         params: { userId, roomId, weekId },
         body: { email },
