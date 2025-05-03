a990ff350711117d76d60c4dea91bc6176ef9ff8
RossNeyman <ross.neyman@gmail.com>
2025-05-03 10:59:40 -0400

    created page for account settings

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,12 +12,14 @@ import { Home } from './Home.tsx';
 import { Signup } from './Signup.tsx';
 import { ForgotPassword } from './forgotPassword.tsx';
 import {ResetPassword} from './resetPassword.tsx';
+import { AccountSettings } from './accountSettings.tsx';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
+          <Route path="/account" element={<AccountSettings />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/scanner" element={<QrScannerPage />} />
