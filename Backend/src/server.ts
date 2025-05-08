b9c7c0a6b5a878351e11fcde76260bf9cbfde0f1
RossNeyman <ross.neyman@gmail.com>
2025-05-08 01:57:38 -0400

    updated port to read properly

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 
 //8000 is the default port, unless otherwise specified in the environment variables
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
 
 export const backend = https.onRequest(app);
