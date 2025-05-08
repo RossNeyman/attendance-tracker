9966982cb3df08b9babb6a49c698ac07f75f8381
RossNeyman <ross.neyman@gmail.com>
2025-05-08 00:06:10 -0400

    cloud functions

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,5 +1,9 @@
-import app from "./app.ts";
+import { https } from "firebase-functions";
+import app from "./app.js";
+
 
 //8000 is the default port, unless otherwise specified in the environment variables
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
+
+export const backend = https.onRequest(app);
