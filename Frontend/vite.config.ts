1ddffef733f9e5ade95e1998e2ff70a7ee8ccb09
RossNeyman <ross.neyman@gmail.com>
2025-05-10 13:46:52 -0400

    added backend url to .env file to be used in dev environment

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig} from 'vite';
+import { defineConfig, loadEnv } from 'vite'; 
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-export default defineConfig(() => {
-  //const env = loadEnv(mode, process.cwd(), '');
-  //const backendUrl = env.BACKEND_URL || 'https://localhost:8080'; 
+
+
+export default defineConfig(({ mode }) => { 
+  //this is used to load backend url from .env file during development
+  //this is not used in production as firebase.json knows to route the requests to the backend
+  const env = loadEnv(mode, process.cwd(), '');
+  const backendUrl = env.VITE_BACKEND_URL; 
 
   return {
   plugins: [react()],
@@ -24,26 +28,22 @@ export default defineConfig(() => {
   },
   server: {
     proxy: {
-      // '/students': {
-      //   target: backendUrl,
-      //   changeOrigin: true,
-      // },
-      // '/logs': {
-      //   target: backendUrl,
-      //   changeOrigin: true,
-      // },
-      // '/weeks': {
-      //   target: backendUrl,
-      //   changeOrigin: true,
-      // },
-      // '/rooms': {
-      //   target: `${backendUrl}/rooms`,
-      //   changeOrigin: true,
-      // },
-      // '/dogs': {
-      //   target: dogApiKey,
-      //   changeOrigin: true,
-      // },
+      '/students': {
+        target: backendUrl,
+        changeOrigin: true,
+      },
+      '/logs': {
+        target: backendUrl,
+        changeOrigin: true,
+      },
+      '/weeks': {
+        target: backendUrl,
+        changeOrigin: true,
+      },
+      '/rooms': {
+        target: backendUrl,
+        changeOrigin: true,
+      },
   },
   }
 };
