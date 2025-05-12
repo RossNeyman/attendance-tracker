2184b8a5e990559b0641a29627b6c2ead6ff5bd8
RossNeyman <ross.neyman@gmail.com>
2025-05-11 23:10:15 -0400

    cleaned up backend

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,33 +1,8 @@
-//import path from 'path';
-//import dotenv from 'dotenv';
-//import { fileURLToPath } from 'url';
 import admin from 'firebase-admin';
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const dotenvResult = dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
-// if (dotenvResult.error) {
-//   console.error('Error loading .env file:', dotenvResult.error);
-// }
-
-// try {
-//   if (!admin.apps.length) {
-//     admin.initializeApp();
-//     console.log('Firebase Admin SDK initialized successfully.');
-//   } else {
-//     admin.app(); 
-//     console.log('Firebase Admin SDK already initialized.');
-//   }
-// } catch (error) {
-//   console.error('Error initializing Firebase Admin SDK:', error);
-//   console.error('GOOGLE_APPLICATION_CREDENTIALS at error:', process.env.GOOGLE_APPLICATION_CREDENTIALS);
-// }
 admin.initializeApp();
-//Export singleton db connection
 const db = getFirestore();
 export default db;
 
