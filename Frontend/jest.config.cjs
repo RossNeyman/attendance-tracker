c99dc3e55a46f500e42f57b47f41c9b6657c7295
RossNeyman <ross.neyman@gmail.com>
2025-05-12 16:32:58 -0400

    UseLoginLogic unit tests

--- Frontend/jest.config.cjs
+++ Frontend/jest.config.cjs
@@ -1,9 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom', 
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'cjs'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'cjs'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
   },
-  testMatch: ['**/__tests__/**/*.(ts|tsx|js)', '**/?(*.)+(spec|test).(ts|tsx|js|cjs)'],
+  testMatch: ['**/__tests__/**/*.(ts|tsx|js|jsx)', '**/?(*.)+(spec|test).(ts|tsx|js|jsx|cjs)'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], 
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
 };
